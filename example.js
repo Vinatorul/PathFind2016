f31e1e2df316936afd126c08e13226d8eacb8481
Alexander Kuvaev <alexander@kuvaev.me>
2016-03-18 23:11:22 +0000

    Deploy build 34

---
+++ example.js
@@ -0,0 +1,13 @@
+angular.module('foundationDemoApp', ['mm.foundation']);
+angular.module('foundationDemoApp').controller('ButtonsCtrl', function ($scope) {
+
+  $scope.singleModel = 1;
+
+  $scope.radioModel = 'Middle';
+
+  $scope.checkModel = {
+    left: false,
+    middle: true,
+    right: false
+  };
+});
