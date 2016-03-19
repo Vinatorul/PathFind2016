f6ce655497d7e715a81cf7ae43960596ec97754d
Alexander <alexander@kuvaev.me>
2016-03-19 03:33:33 +0300

    fork me ribbon and qr code

--- example.js
+++ example.js
@@ -1,13 +1,1 @@
-angular.module('foundationDemoApp', ['mm.foundation']);
-angular.module('foundationDemoApp').controller('ButtonsCtrl', function ($scope) {
-
-  $scope.singleModel = 1;
-
-  $scope.radioModel = 'Middle';
-
-  $scope.checkModel = {
-    left: false,
-    middle: true,
-    right: false
-  };
-});
+angular.module('pathFind2016', ['mm.foundation']);:
